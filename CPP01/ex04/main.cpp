57c239921bb04aeba2d09c7ee2c717a9c105ff14
Ismail Alashqar <ismail.alashqar@gmail.com>
2025-08-03 18:31:03 +0400

    ex04 done

--- CPP01/ex04/main.cpp
+++ CPP01/ex04/main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ialashqa <ialashqa@student.42.fr>          +#+  +:+       +#+        */
+/*   By: ismailalashqar <ismailalashqar@student.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/29 12:37:14 by ialashqa          #+#    #+#             */
-/*   Updated: 2025/08/03 17:07:58 by ialashqa         ###   ########.fr       */
+/*   Updated: 2025/08/03 18:29:23 by ismailalash      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,12 @@
 #include <fstream>
 #include <string>
 
-static int open_fstreams(std::fstream& oldfile, std::fstream& newfile, char *fileName)
+static int open_fstreams(std::fstream& oldfile, std::fstream& newfile, const std::string& fileName)
 {
-	std::string outputFileName = std::string(fileName) + ".replace";
-    newfile.open(fileName, std::ios::in);
-    oldfile.open(outputFileName, std::ios::out);
+	std::string outputFileName = fileName + ".replace";
+    
+    oldfile.open(fileName, std::ios::in);
+    newfile.open(outputFileName, std::ios::out);
 
     if (!oldfile.is_open() || !newfile.is_open())
     {
@@ -28,14 +29,13 @@ static int open_fstreams(std::fstream& oldfile, std::fstream& newfile, char *fil
     return (0);
 }
 
-static void replace(std::string line, std::string s1, std::string s2)
+static void replace(std::string& line, const std::string& s1, const std::string& s2)
 {
     size_t begin = line.find(s1);
-    size_t len = s1.length();
 
     while (begin != std::string::npos)
     {
-        line.replace(begin, len, s2);
+        line.replace(begin, s1.length(), s2);
         begin = line.find(s1, begin + s2.length());
     }
 }
@@ -48,28 +48,22 @@ int main(int ac, char **av)
     std::string     s1;
     std::string     s2;
 
-    if (ac != 4)
-}
-
-
-
-	if (argc != 4 || argv[1][0] == '\0' || argv[2][0] == '\0' || argv[3][0] == '\0')
-	{
-		std::cout << "Invalid arguments: Program takes 3 non-empty arguments!" << std::endl;
-		return (-1);
-	}
-
-	fname = argv[1];
-	s1 = strdup(argv[2]);
-	s2 = strdup(argv[3]);
-
-	if (open_fstreams(istream, ostream, fname))
-		return (-1);
-	while (getline(istream, line))
-	{
-		replace(line, s1, s2);
-		ostream << line << std::endl;
-	}
-	istream.close();
-	ostream.close();
+    if (ac != 4 || av[1][0] == '\0' || av[2][0] == '\0' || av[3][0] == '\0')
+    {
+        std::cout << "Invalid arguments: Program only takes 3 arguments\n";
+        return (-1);
+    }
+    filename = av[1];
+    s1 = av[2];
+    s2 = av[3];
+    if (open_fstreams(oldfile, newfile, filename) == -1)
+        return (-1);
+    while (getline(oldfile, line))
+    {
+        replace(line, s1, s2);
+        newfile << line;
+    }
+    oldfile.close();
+    newfile.close();
+    return (0);
 }
