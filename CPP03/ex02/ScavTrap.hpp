459ef510617e36a68eef8da601e2e549f9854276
Ismail Alashqar <ismail.alashqar@gmail.com>
2025-09-13 23:04:15 +0400

    added ex02

---
+++ CPP03/ex02/ScavTrap.hpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScavTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ismailalashqar <ismailalashqar@student.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/13 18:41:55 by ismailalash       #+#    #+#             */
+/*   Updated: 2025/09/13 19:09:28 by ismailalash      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SCAVTRAP_HPP
+# define SCAVTRAP_HPP
+
+# include "ClapTrap.hpp"
+
+class ScavTrap : public ClapTrap 
+{
+    public:
+        ScavTrap();
+        ScavTrap(std::string name);
+        ScavTrap(const ScavTrap &copy);
+        ~ScavTrap();
+
+        ScavTrap &operator=(const ScavTrap &copy);
+
+        void attack(const std::string &target);
+        void guardGate();
+};
+
+#endif
