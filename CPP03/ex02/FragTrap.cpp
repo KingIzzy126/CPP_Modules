de2a00c6d62669b2734ac262726b957fb83efbe7
Ismail Alashqar <ismail.alashqar@gmail.com>
2025-09-14 10:29:50 +0400

    ex02 almost completed

--- CPP03/ex02/FragTrap.cpp
+++ CPP03/ex02/FragTrap.cpp
@@ -6,7 +6,39 @@
 /*   By: ismailalashqar <ismailalashqar@student.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/13 23:04:00 by ismailalash       #+#    #+#             */
-/*   Updated: 2025/09/13 23:04:01 by ismailalash      ###   ########.fr       */
+/*   Updated: 2025/09/13 23:27:50 by ismailalash      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "FragTrap.hpp"
+
+FragTrap::FragTrap() {
+    hitPoints = 100;
+    energyPoints = 100;
+    attackDamage = 30;
+    std::cout << "--Default FragTrap constructor has joined the arena--\n";
+}
+
+FragTrap::~FragTrap() {
+    std::cout << "--BIG BOY FragTrap " << getName() << " left the arena--\n";
+}
+
+FragTrap::FragTrap(std::string name) : ClapTrap(name) {
+    std::cout << "--BIG BOY FragTrap " << name << " has joined the arena--\n";
+}
+
+FragTrap::FragTrap(const FragTrap &copy) : ClapTrap(copy) {
+    std::cout << "--BIG BOY FragTrap copy constructor called--\n";
+}
+
+FragTrap &FragTrap::operator=(const FragTrap &copy) {
+    std::cout << "--BIG BOY FragTrap assignment operator called--\n";
+    if (this != &copy) {
+        ClapTrap::operator=(copy); // Call parent assignment operator
+    }
+    return *this;
+}
+
+void FragTrap::highFivesGuys(void) {
+    std::cout << "--BIG BOY FragTrap high fives guys!--" << std::endl;
+}
--- CPP03/ex02/FragTrap.hpp
+++ CPP03/ex02/FragTrap.hpp
@@ -6,7 +6,25 @@
 /*   By: ismailalashqar <ismailalashqar@student.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/13 23:03:55 by ismailalash       #+#    #+#             */
-/*   Updated: 2025/09/13 23:03:56 by ismailalash      ###   ########.fr       */
+/*   Updated: 2025/09/13 23:21:25 by ismailalash      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef FRAGTRAP_HPP
+# define FRAGTRAP_HPP
+
+# include "ClapTrap.hpp"
+
+class FragTrap : public ClapTrap {
+    public:
+        FragTrap();
+        ~FragTrap();
+        FragTrap(std::string name);
+        FragTrap(const FragTrap &copy);
+
+        FragTrap &operator=(const FragTrap &copy);
+        
+        void highFivesGuys(void);
+};
+
+#endif
