ffdc1d765e906f660707e64443cf43bbc2186eba
Ismail Alashqar <ismail.alashqar@gmail.com>
2026-03-31 19:44:29 +0400

    done

---
+++ CPP07/ex00/Main.cpp
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ismailalashqar <ismailalashqar@student.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/27 16:41:57 by ismailalash       #+#    #+#             */
+/*   Updated: 2026/03/27 16:42:14 by ismailalash      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Whatever.hpp"
+
+int main( void ) 
+{ 
+    int a = 2;
+    int b = 3;
+    
+    ::swap( a, b );
+    std::cout << "a = " << a << ", b = " << b << std::endl;
+    std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+    std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+    
+    std::string c = "chaine1";
+    std::string d = "chaine2";
+    
+    ::swap(c, d);
+    std::cout << "c = " << c << ", d = " << d << std::endl;
+    std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+    std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+    return 0; 
+}
---
+++ CPP07/ex00/Whatever.hpp
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Whatever.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ismailalashqar <ismailalashqar@student.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/27 16:40:57 by ismailalash       #+#    #+#             */
+/*   Updated: 2026/03/28 15:59:39 by ismailalash      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef WHATEVER_HPP
+# define WHATEVER_HPP
+
+#include <iostream>
+
+template<typename T>
+void swap(T &a, T &b)
+{
+    T temp = a;
+    a = b;
+    b = temp;
+}
+
+template<typename T>
+T min(const T &a, const T &b)
+{
+    if (a < b)
+        return a;
+    else
+        return b;
+}
+
+template<typename T>
+T max(const T &a, const T &b)
+{
+    if (a > b)
+        return a;
+    else
+        return b;   
+}
+#endif
